59559d24d42a5988ed6545fb7003e00cb14d14ce
Laura <laura@wiese2.org>
2026-05-31 16:55:45 +0200

    cleanup fnv helpers

--- ast/ast.go
+++ ast/ast.go
@@ -8,6 +8,11 @@ import (
 	"github.com/coalaura/lugo/token"
 )
 
+const (
+	FnvOffset64 = 14695981039346656037
+	FnvPrime64  = 1099511628211
+)
+
 // NodeKind defines what type of syntax this node represents.
 type NodeKind uint8
 
@@ -399,14 +404,30 @@ func computeLineOffsets(source []byte, lines []uint32) []uint32 {
 
 // HashBytes implements the FNV-1a 64-bit hash algorithm for zero-alloc map keys
 func HashBytes(b []byte) uint64 {
-	var hash uint64 = 14695981039346656037
+	var hash uint64 = FnvOffset64
 
 	if len(b) > 0 {
 		_ = b[len(b)-1] // BCE
 
 		for i := range b {
 			hash ^= uint64(b[i])
-			hash *= 1099511628211
+			hash *= FnvPrime64
+		}
+	}
+
+	return hash
+}
+
+// HashString implements the FNV-1a 64-bit hash algorithm for zero-alloc strings
+func HashString(s string) uint64 {
+	var hash uint64 = FnvOffset64
+
+	if len(s) > 0 {
+		_ = s[len(s)-1] // BCE
+
+		for i := range len(s) {
+			hash ^= uint64(s[i])
+			hash *= FnvPrime64
 		}
 	}
 
@@ -416,14 +437,14 @@ func HashBytes(b []byte) uint64 {
 // HashBytesConcat computes the FNV-1a hash of multiple byte slices separated by a dot,
 // without allocating a new concatenated slice on the heap.
 func HashBytesConcat(a, sep, b []byte) uint64 {
-	var hash uint64 = 14695981039346656037
+	var hash uint64 = FnvOffset64
 
 	if len(a) > 0 {
 		_ = a[len(a)-1] // BCE
 
 		for i := range a {
 			hash ^= uint64(a[i])
-			hash *= 1099511628211
+			hash *= FnvPrime64
 		}
 	}
 
@@ -432,7 +453,7 @@ func HashBytesConcat(a, sep, b []byte) uint64 {
 
 		for i := range sep {
 			hash ^= uint64(sep[i])
-			hash *= 1099511628211
+			hash *= FnvPrime64
 		}
 	}
 
@@ -441,7 +462,7 @@ func HashBytesConcat(a, sep, b []byte) uint64 {
 
 		for i := range b {
 			hash ^= uint64(b[i])
-			hash *= 1099511628211
+			hash *= FnvPrime64
 		}
 	}
 
